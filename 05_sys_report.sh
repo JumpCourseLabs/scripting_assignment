7779c0d6d6f023936607edef0678577664407aae
Michael Phelps <michaeljphelps1978@gmail.com>
2022-03-07 13:25:30 -0600

    System report complete

---
+++ 05_sys_report.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Generates a txt file with a system report
+
+# System Report
+greentext="\033[32m"
+bold="\033[1m"
+normal="\033[0m"
+freemem=$(free -m / | grep -E "\/$" | awk '{print $4}')
+logdate=$(date +"%Y%m%d")
+ipaddress=$(ip a | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+logfile=report_"$logdate".log
+
+echo -e $bold"Quick system report for "$greentext"$HOSTNAME"$normal
+printf "\tHostName:\t%s\n" $HOSTNAME
+printf "\tSystem IP Addresses:\t%s\n" $ipaddress
+printf "\tFree memory:\t%s\n" $freemem
+printf "\tNumber of CPU Cores: \t%s\n" $(nproc)
+printf "\tGenerated on:\t%s\n" $(date +"%m/%d/%y")
+echo -e $greentext"A summary of this info has been saved to $logfile"$normal
+
+cat <<-EOF >$logfile
+
+EOF
+
+printf "\tGenerated on:\t%s\n" $(date +"%m/%d/%y") >>$logfile
+
+printf "\tHostName:\t%s\n" $HOSTNAME >>$logfile
+printf "\tSystem IP Addresses:\t%s\n" $ipaddress >>$logfile
+printf "\tFree memory:\t%s\n" $freemem >>$logfile
+printf "\tNumber of CPU Cores: \t%s\n" $(nproc) >>$logfile
+printf "\tGenerated on:\t%s\n" $(date +"%m/%d/%y") >>$logfile
