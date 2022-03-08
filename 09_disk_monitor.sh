36f5b29801a2b06def66ec72af0d41f28898978d
Michael Phelps <michaeljphelps1978@gmail.com>
2022-03-08 16:37:15 -0600

    Finished

--- 09_disk_monitor.sh
+++ 09_disk_monitor.sh
@@ -4,7 +4,7 @@ dusage=$(df -Ph | grep -vE '^tmpfs|cdrom|mapper\Filesystem' | sed s/%//g | awk '
 fscount=$(echo "$dusage" | wc -l)
 
 checkstats() {
-  if [ $fscount -ge 10 ]; then
+  if [ "$fscount" -ge 10 ]; then
     echo "$dusage" | "Disk Space Alert On $(hostname) at $(date +"%T")" >>disk_alert_$(date +"%Y%m%d").log
   else
     echo "Disk usage is UNDER threshold on $(hostname) at $(date +"%T")" >>disk_alert_$(date +"%Y%m%d").log
